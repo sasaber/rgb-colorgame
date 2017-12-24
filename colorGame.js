4eaad26019bc6d70cdd282c5cccb3189cffa7c49
sasaber <s.ahmsab@gmail.com>
2017-12-24 01:14:10 -0800

    easy/hard modes

--- colorGame.js
+++ colorGame.js
@@ -1,10 +1,61 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+
+var colors = generateRandomColors(numSquares);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetBtn = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easy");
+var hardBtn = document.querySelector("#hard");
+// add event listener to easy btn
+easyBtn.addEventListener("click", function(){
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++){
+		// if there is a color at this index
+		if (colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+// add event listener to hard btn
+hardBtn.addEventListener("click", function(){
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++){
+		// if there is a color at this index
+		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "block";
+	}
+});
+
+// add event listener to reset btn
+resetBtn.addEventListener("click", function(){
+	// generate all new colors
+	colors = generateRandomColors(numSquares);
+	// pick a new random 
+	pickedColor = pickColor();
+	// change colorDisplay to match picked color
+	colorDisplay.textContent = pickedColor;
+	// change the colors of the squares
+	for (var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -18,6 +69,7 @@ for (var i = 0; i < squares.length; i++){
 		// compare color to picked color
 		if (clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
+			resetBtn.textContent = "Play Again?";
 			changeColors(clickedColor);
 			h1.style.backgroundColor = clickedColor;
 		} else {
